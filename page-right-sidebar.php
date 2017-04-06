81ce9239aea463e14eb9f49f13a5ef006243aacf
SaraJerre <sara.jerre@hotmail.com>
2017-04-07 00:55:15 +0200

    ... much more scss/css and DONE!

--- page-right-sidebar.php
+++ page-right-sidebar.php
@@ -7,7 +7,7 @@ get_header();
 
 <div class="container">
   <div class="row">
-    <div class="col-md-10 offset-md-1">
+    <div class="col-md-6 offset-md-1">
 
 <?php
  if ( have_posts() ) : while ( have_posts() ) : the_post();
--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,9 @@
 <div class="primary-sidebar">
 	<ul class="list-unstyled">
 		<?php 
-			if ( is_active_sidebar('primary') ) {
+			if ( is_active_sidebar('sidebar-primary') ) {
 				get_search_form(); 
-				dynamic_sidebar('primary');
+				dynamic_sidebar('sidebar-primary');
 			}
 		?>
 	</ul><!-- /.list-unstyled -->
